87a986cb5d2597f5961fb0d9814176758ec68922
yavictoria <privateaccvictoria@gmail.com>
2022-11-25 12:32:17 +0200

    Victoria's work

---
+++ js.js
@@ -0,0 +1,43 @@
+// function Ingredients() {
+//     const ingredient = document.getElementById("food").value.toLowerCase();
+
+//     if (ingredient == "sause"){
+//         document.getElementById('sause').style.visibility="visible";
+//     }
+    
+//     if (ingredient == "sausage"){
+//         document.getElementById('sausage').style.visibility="visible";
+//     }
+
+//     document.getElementById('food').value = ' '; пробний варіант з вводом інгредієнта
+
+function Sause() {
+     document.getElementById('sause').style.visibility="visible";
+}
+    
+function Sausages() {
+    document.getElementById('sausages').style.visibility="visible";
+}
+
+function Cheese() {
+    document.getElementById('cheese').style.visibility="visible";
+}
+
+function Shrooms() {
+    document.getElementById('shrooms').style.visibility="visible";
+}
+    // switch (ingredient) {
+    //     case "sause":
+    //         alert('Good start');
+    //         break;
+    //     case "sausage":
+    //         alert('I love it too!');
+    //         break;
+    //     default:
+    //         return;
+    // }
+    
+    // const element = document.querySelector('.' + ingredient);
+    // element.classList.add('visible');
+    // }  
+    // теж пробний варіант з вводом
